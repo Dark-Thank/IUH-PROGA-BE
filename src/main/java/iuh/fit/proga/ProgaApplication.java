19ef8522e1059df0416d6461e9447d6cf13b7b94
Lưu Ngọc Cao Sơn <156668941+luungoccaoson@users.noreply.github.com>
2026-07-21 11:11:16 +0700

    Initial project

---
+++ src/main/java/iuh/fit/proga/ProgaApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.proga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iuh/fit/proga/ProgaApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.proga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
