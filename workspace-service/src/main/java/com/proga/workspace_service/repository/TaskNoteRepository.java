2b9c9b0c47a15c65a665ff33e0847d9f28c420b9
Lưu Ngọc Cao Sơn <156668941+luungoccaoson@users.noreply.github.com>
2026-07-21 14:35:17 +0700

    feat: create JpaRepository interfaces for workspace-service

---
+++ workspace-service/src/main/java/com/proga/workspace_service/repository/TaskNoteRepository.java
@@ -0,0 +1,13 @@
+package com.proga.workspace_service.repository;
+
+import com.proga.workspace_service.model.TaskNote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TaskNoteRepository extends JpaRepository<TaskNote, Long> {
+    List<TaskNote> findByTaskIdOrderByCreatedAtAsc(UUID taskId);
+}
